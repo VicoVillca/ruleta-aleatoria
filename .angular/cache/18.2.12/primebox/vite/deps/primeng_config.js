6f051e4c4cbb7d22ebb668e6356b475338a6b2e7
abraham.villca <vicovillca@hotmail.com>
2026-01-09 18:57:12 -0400

    Auto-generated commit

---
+++ .angular/cache/18.2.12/primebox/vite/deps/primeng_config.js
@@ -0,0 +1,17 @@
+import {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+} from "./chunk-OQE6EJZZ.js";
+import "./chunk-ODR75C7O.js";
+import "./chunk-5UDK7ABK.js";
+import "./chunk-SLFMBYBS.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+};
+//# sourceMappingURL=primeng_config.js.map
